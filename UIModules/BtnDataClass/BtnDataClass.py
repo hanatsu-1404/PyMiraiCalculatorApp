27a076190c3e0788a8f7942c0adc43277c86228f
Karon-byte <69085286+Karon-byte@users.noreply.github.com>
2021-03-13 01:25:36 +0900

    Delete UIModules directory
    Add a new directory named "Modules", so this directory isn't needed.

--- UIModules/BtnDataClass/BtnDataClass.py
+++
@@ -1,6 +0,0 @@
-class BtnData:
-    def __init__(self,BtnDispVal,BtnCmd,ShiftDispVal="",ShiftCmd=None):
-        self.BtnDispVal = BtnDispVal
-        self.ShiftDispVal = ShiftDispVal
-        self.BtnCmd = BtnCmd
-
