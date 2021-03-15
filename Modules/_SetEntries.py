b2cfd3f851babeb848b09cbeb09e28345f50bc18
Karon-byte <69085286+Karon-byte@users.noreply.github.com>
2021-03-16 08:57:56 +0900

    Delete Modules directory

--- Modules/_SetEntries.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-def SetEntry(self):
-    self.EntFrame = Frame(self.Root)
-    self.TextWid = Text(self.EntFrame)
-    self.EntFrame.pack(side=LEFT,expand=1,fill=BOTH)
-    self.TextWid.pack(expand=1,fill=BOTH)
